e7d542a7b676326442377e5acf43d41807c186c6
madhur.kumar <madhur.kumar@outlook.com>
2021-01-24 23:13:43 -0500

    spring play

---
+++ src/main/java/fantastic/octo/umbrella/UmbrellaApplication.java
@@ -0,0 +1,13 @@
+package fantastic.octo.umbrella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbrellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbrellaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fantastic/octo/umbrella/UmbrellaApplicationTests.java
@@ -0,0 +1,13 @@
+package fantastic.octo.umbrella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbrellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
